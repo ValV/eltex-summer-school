da59c51a9c598bf7c84d64db5cb6ffe8a141a19e
Vladimir Valeyev <ValV@users.noreply.github.com>
2017-06-25 09:09:16 +0600

    Fix structure field attributes

--- task-02/fix.c
+++ task-02/fix.c
@@ -2,7 +2,7 @@
 
 void main(int argc, char *argv[]) {
     char temp[10] = {'a', 0, 0, 0, 0, 'b', 0, 0, 0, 0};
-    struct {
+    struct __attribute__((packed)) {
         char ch;
         int num;
     } *fix = temp;
