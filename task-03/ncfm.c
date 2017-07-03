9aeaf0dba98291d1f6bc6047372ddd53a49d8eb8
Vladimir Valeyev <ValV@users.noreply.github.com>
2017-07-04 02:44:01 +0600

    Fix panels colors

--- task-03/ncfm.c
+++ task-03/ncfm.c
@@ -3,6 +3,7 @@
 int main(int argc, char *argv[]) {
     /* Variables */
     WINDOW *windows[3];
+    WINDOW *focus_window;
     PANEL *panels[3];
     PANEL *focus_panel;
     int input = 0;
@@ -20,14 +21,20 @@ int main(int argc, char *argv[]) {
     init_pair(3, COLOR_CYAN, 0);
 
     /* Create windows */
-    attron(COLOR_PAIR(1));
     windows[0] = newwin(LINES - 3, COLS / 2, 0, 0);
-    box(windows[0], 0, 0);
     windows[1] = newwin(LINES - 3, COLS / 2, 0, COLS / 2);
-    box(windows[1], 0, 0);
     windows[2] = newwin(3, COLS, LINES - 3, 0);
+
+    /* Draw frames */
+    wattron(windows[0], COLOR_PAIR(1));
+    wattron(windows[1], COLOR_PAIR(1));
+    wattron(windows[2], COLOR_PAIR(1));
+    box(windows[0], 0, 0);
+    box(windows[1], 0, 0);
     box(windows[2], 0, 0);
-    attroff(COLOR_PAIR(1));
+    wattroff(windows[0], COLOR_PAIR(1));
+    wattroff(windows[1], COLOR_PAIR(1));
+    wattroff(windows[2], COLOR_PAIR(1));
 
     /* Create panels */
     panels[0] = new_panel(windows[0]);
@@ -38,33 +45,35 @@ int main(int argc, char *argv[]) {
     set_panel_userptr(panels[0], panels[1]);
     set_panel_userptr(panels[1], panels[0]);
 
-    /* Update stacking order */
+    /* Update focus */
     focus_panel = panels[0];
+    focus_window = panel_window(focus_panel);
     top_panel(focus_panel);
-    wmove(panel_window(focus_panel), 1, 1);
-    update_panels();
+    wmove(focus_window, 1, 1);
+    wattron(focus_window, COLOR_PAIR(2));
+    box(focus_window, 0, 0);
+    wattroff(focus_window, COLOR_PAIR(2));
 
     /* Update the screen */
-    attron(COLOR_PAIR(2));
-    box(panel_window(focus_panel), 0, 0);
-    attroff(COLOR_PAIR(2));
+    update_panels();
     doupdate();
 
     while ((input = getch()) != 'q') {
         switch (input) {
             case '\t':
                 /* Reset highlight */
-                attron(COLOR_PAIR(1));
-                box(panel_window(focus_panel), 0, 0);
-                attroff(COLOR_PAIR(1));
+                wattron(focus_window, COLOR_PAIR(1));
+                box(focus_window, 0, 0);
+                wattroff(focus_window, COLOR_PAIR(1));
                 /* Switch to the next panel */
                 focus_panel = (PANEL *) panel_userptr(focus_panel);
+                focus_window = panel_window(focus_panel);
                 top_panel(focus_panel);
-                wmove(panel_window(focus_panel), 1, 1);
+                wmove(focus_window, 1, 1);
                 /* Highlight */
-                attron(COLOR_PAIR(2));
-                box(panel_window(focus_panel), 0, 0);
-                attroff(COLOR_PAIR(2));
+                wattron(focus_window, COLOR_PAIR(2));
+                box(focus_window, 0, 0);
+                wattroff(focus_window, COLOR_PAIR(2));
                 break;
             default:
                 /* Forward input to the panel window */
