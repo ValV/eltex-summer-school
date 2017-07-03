0b92d74cabdb8459514446829f1b9118aedb2d30
Vladimir Valeyev <ValV@users.noreply.github.com>
2017-07-03 23:31:38 +0600

    Add panels with panel switch routine

---
+++ task-03/ncfm.c
@@ -0,0 +1,80 @@
+#include <panel.h>
+
+int main(int argc, char *argv[]) {
+    /* Variables */
+    WINDOW *windows[3];
+    PANEL *panels[3];
+    PANEL *focus_panel;
+    int input = 0;
+
+    /* Init curses */
+    initscr();
+    start_color();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+
+    /* Init colors */
+    init_pair(1, COLOR_BLUE, 0);
+    init_pair(2, COLOR_WHITE, 0);
+    init_pair(3, COLOR_CYAN, 0);
+
+    /* Create windows */
+    attron(COLOR_PAIR(1));
+    windows[0] = newwin(LINES - 3, COLS / 2, 0, 0);
+    box(windows[0], 0, 0);
+    windows[1] = newwin(LINES - 3, COLS / 2, 0, COLS / 2);
+    box(windows[1], 0, 0);
+    windows[2] = newwin(3, COLS, LINES - 3, 0);
+    box(windows[2], 0, 0);
+    attroff(COLOR_PAIR(1));
+
+    /* Create panels */
+    panels[0] = new_panel(windows[0]);
+    panels[1] = new_panel(windows[1]);
+    panels[2] = new_panel(windows[2]);
+
+    /* Set user pointer to the next focus panel */
+    set_panel_userptr(panels[0], panels[1]);
+    set_panel_userptr(panels[1], panels[0]);
+
+    /* Update stacking order */
+    focus_panel = panels[0];
+    top_panel(focus_panel);
+    wmove(panel_window(focus_panel), 1, 1);
+    update_panels();
+
+    /* Update the screen */
+    attron(COLOR_PAIR(2));
+    box(panel_window(focus_panel), 0, 0);
+    attroff(COLOR_PAIR(2));
+    doupdate();
+
+    while ((input = getch()) != 'q') {
+        switch (input) {
+            case '\t':
+                /* Reset highlight */
+                attron(COLOR_PAIR(1));
+                box(panel_window(focus_panel), 0, 0);
+                attroff(COLOR_PAIR(1));
+                /* Switch to the next panel */
+                focus_panel = (PANEL *) panel_userptr(focus_panel);
+                top_panel(focus_panel);
+                wmove(panel_window(focus_panel), 1, 1);
+                /* Highlight */
+                attron(COLOR_PAIR(2));
+                box(panel_window(focus_panel), 0, 0);
+                attroff(COLOR_PAIR(2));
+                break;
+            default:
+                /* Forward input to the panel window */
+                break;
+        }
+        update_panels();
+        doupdate();
+    }
+    endwin();
+
+    return 0;
+}
+/* vim: set et sw=4: */
