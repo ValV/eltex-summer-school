934e773101a0181410b9a81c6c45261acdc0ae14
Vladimir Valeyev <ValV@users.noreply.github.com>
2017-07-05 08:39:56 +0600

    Add menu directory browsing

--- task-03/ncfm.c
+++ task-03/ncfm.c
@@ -1,34 +1,21 @@
 #include <stdlib.h>
+#include <limits.h>
+#include <dirent.h>
 #include <panel.h>
 #include <menu.h>
 
 #define LEFT 0
 #define RIGHT 1
 #define BOTTOM 2
-#define ELNUM 5
 
 typedef struct tagMETA {
     MENU *menu;
     PANEL *next;
 } META;
 
-char *data_a[] = {
-    "Element A",
-    "Element B",
-    "Element C",
-    "Element D",
-    "Element E",
-};
+typedef struct dirent DIRENT;
 
-char *data_b[] = {
-    "Element 1",
-    "Element 2",
-    "Element 3",
-    "Element 4",
-    "Element 5",
-};
-
-ITEM **create_items(char **from_array);
+ITEM **create_items(char *from_path);
 int free_items(ITEM **from_items);
 
 int main(int argc, char *argv[]) {
@@ -39,11 +26,16 @@ int main(int argc, char *argv[]) {
     PANEL *focus_panel;
     WINDOW *menu_windows[2];
     MENU *menus[2];
+    MENU *temp_menu;
+    ITEM *temp_item;
     META *panels_meta[2] = {
         (META *) malloc(sizeof (struct tagMETA)),
         (META *) malloc(sizeof (struct tagMETA))
     };
     int input = 0, num_items = 0, max_x = 0, max_y = 0, i = 0;
+    char path_left[PATH_MAX], path_right[PATH_MAX];
+    char real_path[PATH_MAX], *temp_path;
+    char *menu_selection = ">";
 
     /* Init curses */
     initscr();
@@ -70,8 +62,15 @@ int main(int argc, char *argv[]) {
     }
 
     /* Create menus */
-    menus[LEFT] = new_menu(create_items(data_a));
-    menus[RIGHT] = new_menu(create_items(data_b));
+    realpath(".", path_left);
+    menus[LEFT] = new_menu(create_items(path_left));
+    set_menu_userptr(menus[LEFT], path_left);
+    set_menu_mark(menus[LEFT], menu_selection);
+
+    realpath(".", path_right);
+    menus[RIGHT] = new_menu(create_items(path_right));
+    set_menu_userptr(menus[RIGHT], path_right);
+    set_menu_mark(menus[RIGHT], menu_selection);
 
     /* Create menu sub-windows */
     for (i = 0; i < 2; i ++) {
@@ -94,6 +93,7 @@ int main(int argc, char *argv[]) {
     panels_meta[LEFT]->menu = menus[LEFT];
     panels_meta[LEFT]->next = panels[RIGHT];
     set_panel_userptr(panels[LEFT], panels_meta[LEFT]);
+
     panels_meta[RIGHT]->menu = menus[RIGHT];
     panels_meta[RIGHT]->next = panels[LEFT];
     set_panel_userptr(panels[RIGHT], panels_meta[RIGHT]);
@@ -102,24 +102,25 @@ int main(int argc, char *argv[]) {
     focus_panel = panels[LEFT];
     focus_window = panel_window(focus_panel);
     top_panel(focus_panel);
-    //wmove(focus_window, 1, 1);
     wattron(focus_window, COLOR_PAIR(2));
     box(focus_window, 0, 0);
     wattroff(focus_window, COLOR_PAIR(2));
+
+    /* Post menus */
+    for (i = 0; i < 2; i ++)
+        post_menu(menus[i]);
 
     /* Update the screen */
     update_panels();
     doupdate();
 
-    /* Post menus */
-    for (i = 0; i < 2; i ++) {
-        post_menu(menus[i]);
-        wrefresh(windows[i]);
-    }
-
     while ((input = getch()) != 'q') {
         switch (input) {
+            case 'a':
+            case 'd':
             case '\t':
+            case KEY_LEFT:
+            case KEY_RIGHT:
                 /* Reset highlight */
                 wattron(focus_window, COLOR_PAIR(1));
                 box(focus_window, 0, 0);
@@ -128,25 +129,48 @@ int main(int argc, char *argv[]) {
                 focus_panel = ((META *) panel_userptr(focus_panel))->next;
                 focus_window = panel_window(focus_panel);
                 top_panel(focus_panel);
-                //wmove(focus_window, 1, 1);
                 /* Highlight */
                 wattron(focus_window, COLOR_PAIR(2));
                 box(focus_window, 0, 0);
                 wattroff(focus_window, COLOR_PAIR(2));
                 break;
+            case 'w':
             case KEY_UP:
                 menu_driver(((META *) panel_userptr(focus_panel))->menu,
                         REQ_UP_ITEM);
                 break;
+            case 's':
             case KEY_DOWN:
                 menu_driver(((META *) panel_userptr(focus_panel))->menu,
                         REQ_DOWN_ITEM);
+                break;
+            case 'e':
+            case KEY_ENTER:
+                /* Get temporary facilities */
+                temp_menu = ((META *) panel_userptr(focus_panel))->menu;
+                temp_item = current_item(temp_menu);
+                if (((DIRENT *) item_userptr(temp_item))->d_type == DT_DIR) {
+                    /* Get current menu path and canonicalize it */
+                    temp_path = (char *) menu_userptr(temp_menu);
+                    realpath(temp_path, real_path);
+                    /* Append file (directory) name to the path */
+                    sprintf(temp_path, "%s/%s",
+                            real_path, item_name(temp_item));
+                    /* Hide menu */
+                    unpost_menu(temp_menu);
+                    /* Destroy old menu items */
+                    free_items(menu_items(temp_menu));
+                    /* Set new menu items */
+                    set_menu_items(temp_menu, create_items(temp_path));
+                    /* Show menu */
+                    post_menu(temp_menu);
+                }
                 break;
             default:
                 /* Forward input to the panel window */
                 break;
         }
-        wrefresh(focus_window);
+        //wrefresh(focus_window);
         update_panels();
         doupdate();
     }
@@ -161,26 +185,32 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-ITEM **create_items(char **from_array) {
+ITEM **create_items(char *from_path) {
     ITEM **item_list;
     int num_items = 0;
+    struct dirent **name_list;
+
+    /* Read directory from_path (memory for name_list will be allocated) */
+    num_items = scandir(from_path, &name_list, NULL, alphasort);
+    if (num_items == -1) return (ITEM **) NULL; // error scanning directory
+
     /* Allocate memory for new items */
-    num_items = ELNUM;
     item_list = (ITEM **) calloc(num_items + 1, sizeof (ITEM *));
-    if (item_list == NULL) {
-        /* Error allocating memory */
+    if (item_list == NULL) { // error allocating memory
+        free(name_list);
         return (ITEM **) NULL;
     }
-    /* Create items */
+
+    /* Create items or return if it's not possible to */
     for (int i = 0; i < num_items; i ++) {
-        /* Create item or return if can't do more */
-        if ((item_list[i] = new_item(from_array[i], from_array[i])) == NULL) {
-            /* Error creating element - return that is done */
-            return item_list;
-        }
-        /* Set user pointer if any */
-        set_item_userptr(item_list[i], (void *) NULL);
+        item_list[i] = new_item(name_list[i]->d_name, NULL);
+        if (item_list[i] == NULL) return item_list; // error creating element
+
+        /* Set user pointer to the pointer of the dirent structure */
+        set_item_userptr(item_list[i], name_list[i]);
     }
+    free(name_list);
+
     item_list[num_items] = (ITEM *) NULL;
     return item_list;
 }
@@ -188,9 +218,14 @@ ITEM **create_items(char **from_array) {
 int free_items(ITEM **from_items) {
     int num_free = 0;
     while (from_items[num_free] != (ITEM *) NULL) {
+        /* Free every dirent structure of the item */
+        free((struct dirent *) item_userptr(from_items[num_free]));
+        /* Free the item (item name is already NULL, description points
+         * to menu_userptr, which is global array variable) */
         free_item(from_items[num_free]);
         num_free ++;
     }
     return num_free;
 }
+
 /* vim: set et sw=4: */
